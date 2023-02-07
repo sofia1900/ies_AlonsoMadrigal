65dc0fa81d634dd75ad234e7998d7302a235c092
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-02-07 16:08:48 +0100

    alumno

---
+++ src/com/iesam/domain/models/Alumno.java
@@ -0,0 +1,79 @@
+package com.iesam.domain.models;
+
+public class Alumno {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String direccion;
+    private String madre;
+    private String padre;
+    private String email;
+
+
+    //Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getMadre() {
+        return madre;
+    }
+
+    public void setMadre(String madre) {
+        this.madre = madre;
+    }
+
+    public String getPadre() {
+        return padre;
+    }
+
+    public void setPadre(String padre) {
+        this.padre = padre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
