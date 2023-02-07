ce6886699e274ad3bd88c9b4cc7a28732561c6ec
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-02-07 19:34:13 +0100

    persona

--- src/com/iesam/domain/models/Alumno.java
+++ src/com/iesam/domain/models/Alumno.java
@@ -1,56 +1,12 @@
 package com.iesam.domain.models;
 
-public class Alumno {
-    private String id;
-    private String nombre;
-    private String apellidos;
-    private String dni;
-    private String direccion;
+public class Alumno extends Persona{
     private String madre;
     private String padre;
     private String email;
 
 
     //Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
 
     public String getMadre() {
         return madre;
---
+++ src/com/iesam/domain/models/Persona.java
@@ -0,0 +1,50 @@
+package com.iesam.domain.models;
+
+public class Persona {
+
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String direccion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
